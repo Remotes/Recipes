08170bd578c00a5bf659224f6f18c1f2bfe9dae1
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2012-09-28 13:31:02 -0700

    experimenting with a different recipe format

--- youtube.js
+++ youtube.js
@@ -25,20 +25,21 @@
 		DOWN : 40,
 		ENTER : 13
 	};
-//useful keyCodes
-// left: 37
-// up : 38
-// right : 39
-// down : 40
-// ENTER : 13
+/*useful keyCodes
+	left: 37
+	up : 38
+	right : 39
+	down : 40
+	ENTER : 13
+*/
 
     Remotes
-        .on("swipe-left", function(e){ window.location = 'javascript:var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 37; eventObj.keyCode = 37; document.dispatchEvent(eventObj);'; })
-		.on("swipe-right", function(e){ window.location = 'javascript:var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 39; eventObj.keyCode = 39; document.dispatchEvent(eventObj);'; })
-		.on("swipe-up", function(e){ window.location = 'javascript:var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 38; eventObj.keyCode = 38; document.dispatchEvent(eventObj);';  })
-		.on("swipe-down", function(e){ window.location = 'javascript:var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 40; eventObj.keyCode = 40; document.dispatchEvent(eventObj);'; })
-		.on("tap", function(e){ window.location = 'javascript:var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 13; eventObj.keyCode = 13; document.dispatchEvent(eventObj);'; })
-		.on("hold", function(e){ /* your magic goes here */ })
-		.on("release", function(e){ /* your magic goes here */ });
+        .on("swipe-left", function(e){ var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 37; eventObj.keyCode = 37; document.dispatchEvent(eventObj); })
+		.on("swipe-right", function(e){ var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 39; eventObj.keyCode = 39; document.dispatchEvent(eventObj); })
+		.on("swipe-up", function(e){ var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 38; eventObj.keyCode = 38; document.dispatchEvent(eventObj);  })
+		.on("swipe-down", function(e){ var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 40; eventObj.keyCode = 40; document.dispatchEvent(eventObj); })
+		.on("tap", function(e){ var eventObj = document.createEvent("Events"); eventObj.initEvent("keydown", true, true); eventObj.which = 13; eventObj.keyCode = 13; document.dispatchEvent(eventObj); })
+		.on("hold", function(e){ })
+		.on("release", function(e){ });
 
 })(new Remotes("preview"));
