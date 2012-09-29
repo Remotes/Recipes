8c5963e6602016177481b2772755ceb758431f2d
Timothée Boucher <timothee@gmail.com>
2012-09-28 20:10:09 -0700

    Enable locally served recipes, switch to HTTPS server

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,21 @@
-import SimpleHTTPServer
+import BaseHTTPServer, SimpleHTTPServer
 import SocketServer
+import ssl
 import fileinput
 import re
 from fabric.api import *
 
-def run(port = 8000):
+def run():
+	port = 4443
 	# create recipes.dev.json based on recipes.json
 	print("Creating dev copy of cookbook")
 	local("cp recipes.json recipes.dev.json")
 	for line in fileinput.input("recipes.dev.json", inplace=1):
 		print re.sub(r'("script"\s*:\s*)"https?://.*/([^/]*.js)"',
-				r'\1"http://localhost:'+str(port)+r'/\2"', line),
+				r'\1"https://localhost:'+str(port)+r'/\2"', line),
 
 	# start server
-	Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-	httpd = SocketServer.TCPServer(("", int(port)), Handler)
+	httpd = BaseHTTPServer.HTTPServer(('localhost', port), SimpleHTTPServer.SimpleHTTPRequestHandler)
+	httpd.socket = ssl.wrap_socket (httpd.socket, certfile='localhost.pem', server_side=True)
 	print("Starting web server on port %s (Ctrl+C to stop)"%port)
 	httpd.serve_forever()
