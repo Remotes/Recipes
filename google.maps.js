7a414aa88d2b89495668d3235c7435027e431099
Timothée Boucher <timothee@gmail.com>
2012-10-04 12:12:41 -0700

    Add maps/satellite toggle on tap event

--- google.maps.js
+++ google.maps.js
@@ -29,11 +29,29 @@
 		}
 	}
 
+	function toggleSatellite() {
+		var divs = document.getElementsByTagName('div');
+		var satellite, maps;
+		for (var i = 0; i < divs.length; i++) {
+			if (divs[i].getAttribute("jsprops")=="activityId:4") {
+				maps = divs[i];
+			} else if (divs[i].getAttribute("jsprops")=="activityId:3") {
+				satellite = divs[i];
+			}
+		}
+		if (maps) {
+			activateElement(maps);
+		} else {
+			activateElement(satellite);
+		}
+	}
+
 	Remotes.on("swipe-left", function(e) { swipe('pan_rt'); })
 	.on("swipe-right", function(e) { swipe('pan_lt'); })
 	.on("swipe-down", function(e) { swipe('pan_up'); })
 	.on("swipe-up", function(e) { swipe('pan_down'); })
 	.on("zoom-in", function(e) { zoom('zoom_in'); })
-	.on("zoom-out", function(e) { zoom('zoom_out'); });
+	.on("zoom-out", function(e) { zoom('zoom_out'); })
+	.on("tap", function(e) { toggleSatellite(); });
 
 })(new Remotes("preview"));
