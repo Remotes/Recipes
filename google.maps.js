f02cfcf5ace3bb8789aa600c524ba277adf28685
Timothée Boucher <timothee@gmail.com>
2012-10-03 19:38:07 -0700

    Add pan and zoom (discreet movements) to Google Maps
    Methods of doing so could most likely be refactored in a nicer way but it works, that's all that matters for now :)

--- google.maps.js
+++ google.maps.js
@@ -1,2 +1,39 @@
 (function(Remotes){
+	function activateElement(element) {
+		var myEvent;
+		if (document.createEventObject) {
+			myEvent = document.createEventObject();
+			return element.fireEvent('onclick', myEvent);
+		} else {
+			myEvent = document.createEvent("HTMLEvents");
+			myEvent.initEvent('click', true, true);
+			return !element.dispatchEvent(myEvent);
+		}
+	}
+
+	function swipe(attribute) {
+		var divs = document.getElementsByTagName('div');
+		for (var i = 0; i < divs.length; i++) {
+			if (divs[i].getAttribute("log")==attribute) {
+				activateElement(divs[i]);
+			}
+		}
+	}
+
+	function zoom(attribute) {
+		var divs = document.getElementsByTagName('div');
+		for (var i = 0; i < divs.length; i++) {
+			if (divs[i].getAttribute("guidedhelpid")==attribute) {
+				activateElement(divs[i].children[1]);
+			}
+		}
+	}
+
+	Remotes.on("swipe-left", function(e) { swipe('pan_rt'); })
+	.on("swipe-right", function(e) { swipe('pan_lt'); })
+	.on("swipe-down", function(e) { swipe('pan_up'); })
+	.on("swipe-up", function(e) { swipe('pan_down'); })
+	.on("zoom-in", function(e) { zoom('zoom_in'); })
+	.on("zoom-out", function(e) { zoom('zoom_out'); });
+
 })(new Remotes("preview"));
