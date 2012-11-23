58346244752228c0a0664ab34a8c234340ad237c
Timothée Boucher <timothee@gmail.com>
2012-11-23 11:38:21 -0800

    Refactor to find DOM elements when script is loaded
    Rather than every time an action is performed.

--- google.maps.js
+++ google.maps.js
@@ -1,4 +1,25 @@
-(function(Remotes){
+(function (Remotes) {
+	var arrowUp, arrowRight, arrowDown, arrowLeft, zoomIn, zoomOut, i;
+
+	var divs = document.getElementsByTagName('div');
+	for (i = 0; i < divs.length; i++) {
+		switch (divs[i].getAttribute("log")) {
+			case "pan_up":
+				arrowUp = divs[i];
+				break;
+			case "pan_rt": arrowRight = divs[i]; break;
+			case "pan_down": arrowDown = divs[i]; break;
+			case "pan_lt": arrowLeft = divs[i]; break;
+			default: break;
+		}
+
+		switch (divs[i].getAttribute("guidedhelpid")) {
+			case "zoom_in": zoomIn = divs[i].children[1]; break;
+			case "zoom_out": zoomOut = divs[i].children[1]; break;
+			default: break;
+		}
+	}
+
 	function activateElement(element) {
 		var myEvent;
 		if (document.createEventObject) {
@@ -11,31 +32,12 @@
 		}
 	}
 
-	function swipe(attribute) {
-		var divs = document.getElementsByTagName('div');
-		for (var i = 0; i < divs.length; i++) {
-			if (divs[i].getAttribute("log")==attribute) {
-				activateElement(divs[i]);
-			}
-		}
-	}
-
-	function zoom(attribute) {
-		var divs = document.getElementsByTagName('div');
-		for (var i = 0; i < divs.length; i++) {
-			if (divs[i].getAttribute("guidedhelpid")==attribute) {
-				activateElement(divs[i].children[1]);
-			}
-		}
-	}
-
 	function toggleSatellite() {
-		var divs = document.getElementsByTagName('div');
 		var satellite, maps;
-		for (var i = 0; i < divs.length; i++) {
-			if (divs[i].getAttribute("jsprops")=="activityId:4") {
+		for (i = 0; i < divs.length; i++) {
+			if (divs[i].getAttribute("jsprops") === "activityId:4") {
 				maps = divs[i];
-			} else if (divs[i].getAttribute("jsprops")=="activityId:3") {
+			} else if (divs[i].getAttribute("jsprops") === "activityId:3") {
 				satellite = divs[i];
 			}
 		}
@@ -46,12 +48,34 @@
 		}
 	}
 
-	Remotes.on("swipe-left", function(e) { swipe('pan_rt'); })
-	.on("swipe-right", function(e) { swipe('pan_lt'); })
-	.on("swipe-down", function(e) { swipe('pan_up'); })
-	.on("swipe-up", function(e) { swipe('pan_down'); })
-	.on("zoom-in", function(e) { zoom('zoom_in'); })
-	.on("zoom-out", function(e) { zoom('zoom_out'); })
-	.on("tap", function(e) { toggleSatellite(); });
+	function toggleOverlayedInfo() {
+		console.log('hold');
+		var satellite, maps, traffic, labels;
+		for (i = 0; i < divs.length; i++) {
+			if (divs[i].getAttribute("jsprops")=="activityId:4") {
+				maps = divs[i];
+			} else if (divs[i].getAttribute("jsprops")=="activityId:3") {
+				satellite = divs[i];
+			} else if (divs[i].getAttribute("jsprops") == "activityId:7") {
+				traffic = divs[i];
+			} else if (divs[i].getAttribute("jsprops") == "activityId:1") {
+				labels = divs[i];
+			}
+		}
+		if (maps) {
+			activateElement(labels);
+		} else if (maps) {
+			activateElement(traffic);
+		}
+	}
+
+	Remotes.on("swipe-up", function(e) { activateElement(arrowUp); })
+	.on("swipe-right", function(e) { activateElement(arrowLeft); })
+	.on("swipe-down", function(e) { activateElement(arrowDown); })
+	.on("swipe-left", function(e) { activateElement(arrowRight); })
+	.on("zoom-in", function(e) { activateElement(zoomIn); })
+	.on("zoom-out", function(e) { activateElement(zoomOut); })
+	.on("tap", function(e) { toggleSatellite(); })
+	.on("hold", function(e) { toggleOverlayedInfo(); });
 
 })(new Remotes("preview"));
